bd36fe239a755774d8ecabaf1d27f1e05c26aeb1
Ayush Iyankan <ayushiyankan007@gmail.com>
2023-02-24 17:48:58 +0530

    finish navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 //styles
-import "./App.css";
+import "./App.scss";
 
 //pages
 import Home from "./pages/Home";
--- src/components/home/navbar/Navbar.jsx
+++ src/components/home/navbar/Navbar.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //styles
 import "./Navbar.scss";
 
+//images
+import logo from "../../../assets/images/logo.png";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__sub">
+        <div className="contact">
+          <div className="contact__link">
+            <Link to={"/"} className="link">
+              <p>CONTACT</p>
+            </Link>
+          </div>
+        </div>
+        <div className="central__logo">
+          <div className="logo">
+            <Link to={"/"}>
+              <img src={logo} alt="logo" />
+            </Link>
+          </div>
+        </div>
+        <div className="register">
+          <div className="register__button">
+            <Link to={"/register"}>
+              <button>
+                <p>REGISTER</p>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
