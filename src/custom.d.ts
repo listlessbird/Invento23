69227c1718b4ffc1cc2f624b2987dafdf86b12b9
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-02-26 21:34:57 +0530

    add svgr plugin

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,9 +1,15 @@
-declare module "*.svg" {
-  import * as React from "react";
+// declare module "*.svg" {
+//   import * as React from "react";
 
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
 
-  export default ReactComponent;
+//   export default ReactComponent;
+// }
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
