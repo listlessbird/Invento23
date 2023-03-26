5f092ab831ccac2fdce6480297ebb7ee3facb3a5
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-03-26 21:59:29 +0530

    fix hero text and adjust bg

--- src/components/countdown/countdown.tsx
+++ src/components/countdown/countdown.tsx
@@ -5,24 +5,28 @@ export default function Countdown() {
 
   const countdown = [
     {
+      id: 3,
       title: "Days",
       item: days,
     },
     {
+      id: 2,
       title: "Hours",
       item: hours,
     },
     {
+      id: 1,
       title: "Minutes",
       item: minutes,
     },
     {
+      id: 0,
       title: "Seconds",
       item: seconds,
     },
   ].map((cd) => {
     return (
-      <div className={styles.countdown__box__wrap}>
+      <div className={styles.countdown__box__wrap} key={cd.id}>
         <div className={styles.countdown__box}>
           <span className={styles.countdown__text}>{cd.item}</span>
         </div>
