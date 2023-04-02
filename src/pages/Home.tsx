db44636f0fc59140c7f86427ba65b8f8d173016b
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-04-02 23:12:32 +0530

    feat: footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Footer from "../Sections/Footer"
 import Hero from "../Sections/Hero"
 //components
 import Navbar from "../components/navbar/Navbar"
@@ -7,11 +8,12 @@ import styles from "./Home.module.scss"
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <div className={styles.home}></div>
       <Navbar />
       <Hero />
-    </>
+      <Footer />
+    </div>
   )
 }
 
